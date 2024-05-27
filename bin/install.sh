b1b0a60a8db703315e7bcf4926dfd4d31f0a7603
kravc <kravc1999@gmail.com>
2024-05-27 23:17:57 +0300

    teawet

--- bin/install.sh
+++ bin/install.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/bash
 
+. ./.env
+
 npm ci
 # mv -f .env.frontend .env
 npx supabase gen types typescript --project-id "xasnhtflxlvxipgizzqv" --schema public > types/supabase.ts
--- bin/post-deploy.sh
+++ bin/post-deploy.sh
@@ -3,10 +3,7 @@
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 
-if [ ! -f .env ]
-then
-  export $(cat .env | xargs)
-fi
+. ./.env
 
 sh ./bin/install.sh
 
