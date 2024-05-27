1e263925628101db4f379e41ecab5abeda265a5a
kravc <kravc1999@gmail.com>
2024-05-27 23:06:01 +0300

    hello

--- src/app/dashboard/(personalAccount)/settings/page.tsx
+++ src/app/dashboard/(personalAccount)/settings/page.tsx
@@ -1,13 +1,16 @@
-import EditPersonalAccountName from "@/components/basejump/edit-personal-account-name";
-import {createClient} from "@/lib/supabase/server";
+import EditPersonalAccountName from "@/components/basejump/edit-personal-account-name"
+import { createClient } from "@/lib/supabase/server"
 
 export default async function PersonalAccountSettingsPage() {
-    const supabaseClient = createClient();
-    const {data: personalAccount} = await supabaseClient.rpc('get_personal_account');
+  const supabaseClient = createClient()
+  const { data: personalAccount } = await supabaseClient.rpc(
+    "get_personal_account"
+  )
 
-    return (
-        <div>
-            <EditPersonalAccountName account={personalAccount} />
-        </div>
-    )
+  return (
+    <div>
+      {/* @ts-ignore */}
+      <EditPersonalAccountName account={personalAccount} />
+    </div>
+  )
 }
