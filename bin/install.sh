d0c646ae003474edb558aa6685abfc4f3976c393
kravc <kravc1999@gmail.com>
2024-05-27 23:16:05 +0300

    asddd

--- bin/install.sh
+++ bin/install.sh
@@ -2,5 +2,5 @@
 
 npm ci
 # mv -f .env.frontend .env
-npx supabase gen types typescript --project-id \"xasnhtflxlvxipgizzqv\" --schema public > types/supabase.ts
+npx supabase gen types typescript --project-id "xasnhtflxlvxipgizzqv" --schema public > types/supabase.ts
 npm run build
